c92bc83a1639bc10f3110784de95fb1dc78a381f
PragmaticRobot <yabdelm2@gmail.com>
2016-05-27 17:41:52 -0500

    played around with PCA and factor analysis with varimax rotation for speed columns

--- check_speed_pca.R
+++ check_speed_pca.R
@@ -7,8 +7,13 @@
 #########################
 
 rm(list = ls()) # clear the environment
-library(MASS)
-library(R.matlab)
+require(MASS)
+require(R.matlab)
+require(devtools)
+# require(ggbiplot)
+require(rgl)
+require(nFactors)
+library(FactoMineR)
 
 ##
 LASSO_res <- readMat("lasso_pred.mat", maxLength=NULL, fixNames=TRUE, Verbose=FALSE)
@@ -17,9 +22,10 @@ load("colnames.rda")
 ## restructure the data into a data frame
 # RF: Random Forests
 # LS: LASSO
-################################## IMPORTANT NOTE #################################################
-######### This section ONLY deals with LINEAR results, quadratic models are not included! #########
-###################################################################################################
+################################## IMPORTANT NOTE ########
+#### This section ONLY deals with LINEAR results, 
+#### quadratic models are not included!
+
 gm2 <- c("Intercept",gm)    # add "intercept" to the list of feature names for LASSO
 gverb <- gm                 # verbose version of feature names
 
@@ -29,27 +35,33 @@ gverb[3] <- "Mean Initial Direction Error"; gverb[4] <- "Mean Pre-Movement Speed
 gverb[5] <- "Mean Max Speed"; gverb[6] <- "Mean Hand Path Length"
 gverb[7] <- "Mean Initial Movement Ratio"; gverb[8] <- "Mean Speed Ratio"
 gverb[9] <- "Mean Path Length Ratio"; gverb[10] <- "Mean Number of Speed Peaks"
-gverb[11] <- "Mean Max Perpendicular Distance"; gverb[12] <- "Mean Percent of Movement in the Target Direction"
+gverb[11] <- "Mean Max Perpendicular Distance"; 
+gverb[12] <- "Mean Percent of Movement in the Target Direction"
 gverb[13] <- "Mean Mean Arrest Period Ratio"; gverb[14] <- "Max Reaction Time"
 gverb[15] <- "Max Trial Time"; gverb[16] <- "Max Initial Direction Error"
 gverb[17] <- "Max Pre-Movement Speed"; gverb[18] <- "Max Max Speed"
 gverb[19] <- "Max Hand Path Length"; gverb[20] <- "Max Initial Movement Ratio"
 gverb[21] <- "Max Path Length Ratio"; gverb[22] <- "Max Number of Speed Peaks"
-gverb[23] <- "Max Max Perpendicular Distance"; gverb[24] <- "Max Percent of Movement in the Target Direction"
+gverb[23] <- "Max Max Perpendicular Distance"; 
+gverb[24] <- "Max Percent of Movement in the Target Direction"
 gverb[25] <- "Max Mean Arrest Period Ratio"; gverb[26] <- "Var Reaction Time"
 gverb[27] <- "Var Trial Time"; gverb[28] <- "Var Initial Direction Error"
 gverb[29] <- "Var Pre-Movement Speed"; gverb[30] <- "Var Max Speed"
 gverb[31] <- "Var Hand Path Length"; gverb[32] <- "Var Initial Movement Ratio"
 gverb[33] <- "Var Speed Ratio"; gverb[34] <- "Var Path Length Ratio"
-gverb[35] <- "Var Number of Speed Peaks"; gverb[36] <- "Var Max Perpendicular Distance"
-gverb[37] <- "Var Percent of Movement in the Target Direction"; gverb[38] <- "Var Mean Arrest Period Ratio"
+gverb[35] <- "Var Number of Speed Peaks"; 
+gverb[36] <- "Var Max Perpendicular Distance"
+gverb[37] <- "Var Percent of Movement in the Target Direction"; 
+gverb[38] <- "Var Mean Arrest Period Ratio"
 gverb[39] <- "Age"; gverb[40] <- "Height"; gverb[41] <- "Mass"; 
-gverb[42] <- "Months Post-Stroke"; gverb[43] <- "Sex"; gverb[44] <- "Left Hand Dominant?"
+gverb[42] <- "Months Post-Stroke"; gverb[43] <- "Sex"; 
+gverb[44] <- "Left Hand Dominant?"
 gverb[45] <- "Left Side Affected?"; gverb[46] <- "Dominant Side Affected?"
 gverb[47] <- "Hemorrhagic Stroke?"; gverb[48] <- "Unknown Stroke Location?"
 gverb[49] <- "Cortical Stroke?"; gverb[50] <- "Subcortical Stroke?"
 gverb[51] <- "Brainstem Stroke?"; gverb[52] <- "Error-Augmentation Treatment?"
-gverb[53] <- "Initial Fugl-Meyer Score"; gverb[54] <- "Initial Wolf Motor Function Score"
+gverb[53] <- "Initial Fugl-Meyer Score"; 
+gverb[54] <- "Initial Wolf Motor Function Score"
 gverb[55] <- "Initial Box-and-Blocks Score"
 
 ##
@@ -63,8 +75,10 @@ RF_2 <- data.frame(matrix(vector(),55,100), row.names = gverb)
 RF_3 <- data.frame(matrix(vector(),55,100), row.names = gverb)
 
 
-## put the features in the right order with coefficients (LASSO) and variable importance (RF)
-## after this, each matrix has features for rows, and cross-validation results in columns
+## put the features in the right order with coefficients (LASSO) 
+# and variable importance (RF)
+## after this, each matrix has features for rows, and cross-validation 
+# results in columns
 for (j in 1:100) # loop over cross-validations
 {
   for (i in 1:dim(LASSO_res$LASSO.pred1[[2]])[1])
@@ -73,7 +87,8 @@ for (j in 1:100) # loop over cross-validations
       next 
     }
     else {
-      LASSO_1[LASSO_res$LASSO.pred1[[2]][i,j],j] <- LASSO_res$LASSO.pred1[[3]][i,j]
+      LASSO_1[LASSO_res$LASSO.pred1[[2]][i,j],j] <- 
+        LASSO_res$LASSO.pred1[[3]][i,j]
     }
   }
   ####################################################################
@@ -83,7 +98,8 @@ for (j in 1:100) # loop over cross-validations
       next 
     }
     else {
-      LASSO_2[LASSO_res$LASSO.pred2[[2]][i,j],j] <- LASSO_res$LASSO.pred2[[3]][i,j]
+      LASSO_2[LASSO_res$LASSO.pred2[[2]][i,j],j] <- 
+        LASSO_res$LASSO.pred2[[3]][i,j]
     }
   }
   #####################################################################
@@ -93,7 +109,8 @@ for (j in 1:100) # loop over cross-validations
       next 
     }
     else {
-      LASSO_3[LASSO_res$LASSO.pred3[[2]][i,j],j] <- LASSO_res$LASSO.pred3[[3]][i,j]
+      LASSO_3[LASSO_res$LASSO.pred3[[2]][i,j],j] <- 
+        LASSO_res$LASSO.pred3[[3]][i,j]
     }
   }
   #####################################################################
@@ -110,7 +127,8 @@ for (j in 1:100) # loop over cross-validations
 ##                 Data Prep - Importing           ##
 #####################################################
 
-Data <- readMat("RF_Ready_noscramble.mat", maxLength=NULL, fixNames=TRUE, Verbose=FALSE)
+Data <- readMat("RF_Ready_noscramble.mat", maxLength=NULL, fixNames=TRUE, 
+                erbose=FALSE)
 # setup variables
 Features <- data.frame(Data$red.DesignMat[1:26,2:56])
 Outcomes <- data.frame(Data$y.FM,Data$y.PartsFM,Data$y.WO)
@@ -131,7 +149,8 @@ CombinedNum <- cbind(Features[,1:42],Features[,53:55],Outcomes)
 CombinedFull <- cbind(Features,Outcomes)
 rm(Data,Dpath,i,numfeat)
 
-colnames(Features) <- gsub(" ","",colnames(Features)) # ggpairs doesn't like spaces in varnames
+colnames(Features) <- gsub(" ","",colnames(Features)) # ggpairs 
+                            # doesn't like spaces in varnames
 top <- dim(Features)[2]
 
 ## We want to use the "Features" Data Frame for the next part(s)
@@ -141,7 +160,8 @@ top <- dim(Features)[2]
 ############################################################
 VarNames <- colnames(Features)
 varsToFactor <- VarNames[43:52]
-FeaturesF <- Features # a copy of features with binary variables turned into factors (so the original is fine)
+FeaturesF <- Features # a copy of features with binary variables 
+                      # turned into factors (so the original is fine)
 FeaturesF[varsToFactor] <- lapply(FeaturesF[varsToFactor], factor)
 
 for (i in 43:52)
@@ -161,7 +181,8 @@ yWO = Outcomes$Data.y.WO
 ## 03 - Prepare for quadratic random forests ##
 ###############################################
 df1 <- Features 
-# this is the original copy of Features, where binary variables are still treated as integers
+# this is the original copy of Features, where binary variables 
+# are still treated as integers
 # this allows for the multiplication of binary variables with other variables
 
 df2 <- Features
@@ -177,7 +198,8 @@ for (i in c(1:dim(df1)[2])) {
 }
 colnames(df2) <- c(names2)
 FeaturesQ <- df2               
-# a new data frame containing quadratic predictors with correct column names (thanks Peter!)
+# a new data frame containing quadratic predictors with correct column
+# names (thanks Peter!)
 FeaturesQ[varsToFactor] <- lapply(FeaturesQ[varsToFactor], factor)
 
 for (i in 43:52)
@@ -205,10 +227,35 @@ rm(list=ls()[! ls() %in% c("df1","df2","gverb","gm")])
 ## speed related features in df1:
 ## 4,5,8,10 (spd pks), 17,18,22 (spd pks), 29,30,33,35
 spd_cols <- df1[,c(4,5,8,10,17,18,22,29,30,33,35)]
-spd.pca <- prcomp(spd_cols,center = TRUE, scale. = TRUE)
-plot(spd.pca, type = "l")
-summary(spd.pca)
+spd.pca <- prcomp(spd_cols,center = TRUE, scale. = TRUE, rank. = 3)
+summary(spd.pca) # variance accounted for
+spd.pca$rotation # PC loadings
+plot(spd.pca,type="lines") # scree plot
+spd.pca$x # pca scores (values of observations for rotated data)
+biplot(spd.pca) # plot pc1 against pc2
+# ggbiplot(spd.pca, choices = 1:2,obs.scale = 1, var.scale = 1, 
+         # ellipse = TRUE,circle = TRUE) # a prettier plot of pc1 vs pc2
+plot3d(spd.pca$x[,1:3]) # a 3d plot of the pc1 - pc3
 
+## factor analysis with varimax rotation
+spd.fact <- factanal(spd_cols, 3, rotation = "varimax") # pick first 3 factors
+print(spd.fact, digits = 2, cutoff = 0.3, sort = TRUE) # print the factors
+load <- spd.fact$loadings[,1:2] # factor loadings of first two factors
+plot(load, type = "p") # plot factor loadings
+text(load, labels = names(spd_cols), cex = .7, pos = 3) # label features
+
+## Determining the number of factors to extract
+ev <- eigen(cor(spd_cols)) # get eigenvalues
+ap <- parallel(subject=nrow(spd_cols),var=ncol(spd_cols),
+               rep=100,cent=.05)
+nS <- nScree(x=ev$values, aparallel=ap$eigen$qevpea)
+plotnScree(nS) 
+
+## Using FactoMineR package
+result_spd_pca <- PCA(spd_cols)
+
+
+## whole data PCA
 whole.pca <- prcomp(df1,center=TRUE, scale. = TRUE)
 plot(whole.pca, type="l")
 summary(whole.pca)
