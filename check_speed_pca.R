411a36777c9f8f8ec1ccd755dd13aef11d687bb1
PragmaticRobot <yabdelm2@gmail.com>
2016-06-06 19:42:54 -0500

    Built the factors Did LASSO (linear) and RF (linear) on factors Got predictions with 100-fold CV

--- check_speed_pca.R
+++ check_speed_pca.R
@@ -2,21 +2,103 @@
 ## This script will pool the speed-related features and run PCA on them,
 ## see how much they covary
 
+
+rm(list = ls()) # clear the environment
+
+####################
+## 99 - Functions ##
+####################
+
+cv_RF <- function(feat,out)
+{
+  RF_model <- foreach(ntree = rep(10000,4), .combine = combine, .multicombine=TRUE, 
+                      .packages = "randomForest") %dopar% randomForest(feat,out,ntree=ntree)
+  imp <- sort(RF_model$importance, partial=NULL,decreasing=TRUE, index.return=TRUE)
+  predict <- predict(RF_model, feat, type = "response" , predict.all=FALSE)
+  GRE <- list(predict=predict, Index=imp$ix, imps=imp$x)
+  return(GRE)
+}
+
+cv_mod <- function(feat, out, stand, isWO)
+{
+  if(stand == 1)
+  {
+    lassoMOD <- cv.glmnet(x=as.matrix(feat),y=out, standardize = TRUE)
+  } else {
+    lassoMOD <- cv.glmnet(x=as.matrix(feat),y=out, standardize = FALSE)
+  }
+  cutoff <- min(lassoMOD$cvup)
+  means <- lassoMOD$cvm
+  if(isWO == 1)
+  {
+    ind <- min(which(means<cutoff)) # now we know which model won
+  } else {
+    ind <- max(which(means<cutoff)) # now we know which model won
+  }
+  predict <- predict(lassoMOD, newx=as.matrix(feat), s= lassoMOD$lambda[ind])
+  cc <- coef(lassoMOD$glmnet.fit, s = lassoMOD$lambda[ind])
+  summ <- summary(cc)
+  GRE <- list(predict=predict, Index = summ$i, coefs = summ$x)
+  return(GRE)
+}
+
+## for the cleanup functions flag:
+## 1: predictions
+## 2: indices of winning variables (LASSO) or order of variable importance (RF)
+## 3: value for coefficients of winning variables, or importance of variable (RF)
+cleanup_LASSO <- function(pred1)
+{
+  predictions1 <- pred1[[1]]$predict
+  inds1 <- pred1[[1]]$Index
+  coefs1 <- pred1[[1]]$coefs
+  for (i in 2:100)
+  {
+    predictions1 <- cbind(predictions1,pred1[[i]]$predict)
+    inds1 <- qpcR:::cbind.na(inds1,pred1[[i]]$Index)
+    coefs1 <- qpcR:::cbind.na(coefs1,pred1[[i]]$coefs)
+  }
+  LASSO_pred1 <- list(Predictions=predictions1,Indices=inds1,Coeffs=coefs1)
+  return(LASSO_pred1)
+}
+
+cleanup_RF <- function(pred1)
+{
+  predictions1 <- pred1[[1]]$predict
+  inds1 <- pred1[[1]]$Index
+  imps1 <- pred1[[1]]$imps
+  for (i in 2:100)
+  {
+    predictions1 <- cbind(predictions1,pred1[[i]]$predict)
+    inds1 <- qpcR:::cbind.na(inds1,pred1[[i]]$Index)
+    imps1 <- qpcR:::cbind.na(imps1,pred1[[i]]$imps)
+  }
+  RF_pred1 <- list(Predictions=predictions1,Indices=inds1,Imps=imps1)
+  return(RF_pred1)
+}
+
+
 #########################
 ## Setup and libraries ##
 #########################
 
-rm(list = ls()) # clear the environment
-require(MASS)
-require(R.matlab)
-require(devtools)
-# require(ggbiplot)
-require(rgl)
-require(nFactors)
+par.o <- par()
+library(MASS)
+library(R.matlab)
+library(devtools)
+library(rgl)
+library(nFactors)
 library(FactoMineR)
 library(psych)
+library(foreach)
+library(randomForest)
+library(doParallel)
+library(inTrees)
+library(lars)
+library(glmnet)
+library(coefplot)
 
 ##
+setwd("C:/Users/Yaz/Documents/Clinical_Prediction/")
 LASSO_res <- readMat("lasso_pred.mat", maxLength=NULL, fixNames=TRUE, Verbose=FALSE)
 RF_res <- readMat("RF_pred.mat", maxLength=NULL, fixNames=TRUE, Verbose=FALSE)
 load("colnames.rda")
@@ -218,7 +300,7 @@ for (i in 43:52)
 ## df2: quad data frame with subject x feature
 
 ## remove everything except df1 & 2 and variable names
-rm(list=ls()[! ls() %in% c("df1","df2","gverb","gm")])
+# rm(list=ls()[! ls() %in% c("df1","df2","gverb","gm","yFM","yPartFM","yWO")])
 
 ## Current workspace:
 ## df1 & df2
@@ -250,6 +332,7 @@ ev <- eigen(cor(spd_cols)) # get eigenvalues
 ap <- parallel(subject=nrow(spd_cols),var=ncol(spd_cols),
                rep=100,cent=.05)
 nS <- nScree(x=ev$values, aparallel=ap$eigen$qevpea)
+par(par.o)
 plotnScree(nS) 
 
 ## Using FactoMineR package
@@ -264,11 +347,11 @@ whole.pca$x # PCA scores
 biplot(whole.pca)
 plot3d(whole.pca$x[,1:3])
 # How many factors?
-ev2 <- eigen(cor(df1))
-ap2 <- parallel(subject = nrow(df1), var = ncol(df1),
+ev0 <- eigen(cor(df1))
+ap0 <- parallel(subject = nrow(df1), var = ncol(df1),
                 rep=100,cent = 0.05)
-ns2 <- nScree(x=ev2$values,aparallel = ap2$eigen$qevpea)
-plotnScree(ns2)
+ns0 <- nScree(x=ev0$values,aparallel = ap0$eigen$qevpea)
+plotnScree(ns0)
 # Varimax Rotation
 ### whole.fact <- factanal(df1, 4)
 # since the matrix is singular, we need a different method, using the psych package
@@ -291,7 +374,7 @@ barplot(rev(whole.fact$loadings[,4]), horiz = TRUE, main="UEFM PC4",
 ###########################################################
 #### Now let's look at "rational" grouping of factors #####
 ###########################################################
-
+par(par.o)
 ## Speed-related features: spd.fact
 spd_cols <- df1[,c(4,5,8,10,17,18,22,29,30,33,35)]
 stroke_cols <- df1[,c(42,44,45,46,47,48,49,50,51)] # stroke info
@@ -301,3 +384,131 @@ treat <- df1[,c(52)] # treatment
 launch_cols <- df1[,c(1,3,7,14,16,20,26,28,32)]
 mvmnt_cols <- df1[,c(2,6,9,13,15,19,21,25,27,31,34,38)]
 accuracy_cols <- df1[,c(11,12,23,24,36,37)]
+
+## The Seven Families:
+# spd_cols
+# stroke_cols
+# clinic_cols
+# demog_cols
+# launch_cols
+# mvmnt_cols
+# accuracy_cols
+
+## How many factors we need for each family?
+ev1 <- eigen(cor(spd_cols)) # get eigenvalues
+ev2 <- eigen(cor(stroke_cols)) # get eigenvalues
+ev3 <- eigen(cor(clinic_cols)) # get eigenvalues
+ev4 <- eigen(cor(demog_cols)) # get eigenvalues
+ev5 <- eigen(cor(launch_cols)) # get eigenvalues
+ev6 <- eigen(cor(mvmnt_cols)) # get eigenvalues
+ev7 <- eigen(cor(accuracy_cols)) # get eigenvalues
+
+ap1 <- parallel(subject=nrow(spd_cols),var=ncol(spd_cols), rep=100,cent=.05)
+ap2 <- parallel(subject=nrow(stroke_cols),var=ncol(stroke_cols), rep=100,cent=.05)
+ap3 <- parallel(subject=nrow(clinic_cols),var=ncol(clinic_cols), rep=100,cent=.05)
+ap4 <- parallel(subject=nrow(demog_cols),var=ncol(demog_cols), rep=100,cent=.05)
+ap5 <- parallel(subject=nrow(launch_cols),var=ncol(launch_cols), rep=100,cent=.05)
+ap6 <- parallel(subject=nrow(mvmnt_cols),var=ncol(mvmnt_cols), rep=100,cent=.05)
+ap7 <- parallel(subject=nrow(accuracy_cols),var=ncol(accuracy_cols), rep=100,cent=.05)
+
+nS1 <- nScree(x=ev1$values, aparallel=ap1$eigen$qevpea)
+nS2 <- nScree(x=ev2$values, aparallel=ap2$eigen$qevpea)
+nS3 <- nScree(x=ev3$values, aparallel=ap3$eigen$qevpea)
+nS4 <- nScree(x=ev4$values, aparallel=ap4$eigen$qevpea)
+nS5 <- nScree(x=ev5$values, aparallel=ap5$eigen$qevpea)
+nS6 <- nScree(x=ev6$values, aparallel=ap6$eigen$qevpea)
+nS7 <- nScree(x=ev7$values, aparallel=ap7$eigen$qevpea)
+
+plotnScree(nS1)
+plotnScree(nS2)
+plotnScree(nS3)
+plotnScree(nS4)
+plotnScree(nS5)
+plotnScree(nS6)
+plotnScree(nS7)
+
+## Now we know how many factors:
+# spd_cols 3
+# stroke_cols 4
+# clinic_cols 1
+# demog_cols 1
+# launch_cols 2
+# mvmnt_cols 3
+# accuracy_cols 2
+
+## Let's do factor analysis with the requisite number of factors for each family:
+spd_f <- fa(spd_cols, nfactors = 3, residuals = TRUE, rotate = "varimax", min.err = 0.001, fm="minres")
+stroke_f <- fa(stroke_cols, nfactors = 4, residuals = TRUE, rotate = "varimax", min.err = 0.001, fm="minres")
+clinic_f <- fa(clinic_cols, nfactors = 1, residuals = TRUE, rotate = "varimax", min.err = 0.001, fm="minres")
+demog_f <- fa(demog_cols, nfactors = 1, residuals = TRUE, rotate = "varimax", min.err = 0.001, fm="minres")
+launch_f <- fa(launch_cols, nfactors = 2, residuals = TRUE, rotate = "varimax", min.err = 0.001, fm="minres")
+mvmnt_f <- fa(mvmnt_cols, nfactors = 3, residuals = TRUE, rotate = "varimax", min.err = 0.001, fm="minres")
+accuracy_f <- fa(accuracy_cols, nfactors = 2, residuals = TRUE, rotate = "varimax", min.err = 0.001, fm="minres")
+
+print(spd_f$loadings, digits = 2, sort=FALSE, fixnames = TRUE, cutoff = 0.3)
+print(stroke_f$loadings, digits = 2, sort=FALSE, fixnames = TRUE, cutoff = 0.3)
+print(clinic_f$loadings, digits = 2, sort=FALSE, fixnames = TRUE, cutoff = 0.3)
+print(demog_f$loadings, digits = 2, sort=FALSE, fixnames = TRUE, cutoff = 0.3)
+print(launch_f$loadings, digits = 2, sort=FALSE, fixnames = TRUE, cutoff = 0.3)
+print(mvmnt_f$loadings, digits = 2, sort=FALSE, fixnames = TRUE, cutoff = 0.3)
+print(accuracy_f$loadings, digits = 2, sort=FALSE, fixnames = TRUE, cutoff = 0.3)
+
+spd_fa <- as.data.frame(spd_f$scores)
+stroke_fa <- as.data.frame(stroke_f$scores)
+clinic_fa <- as.data.frame(clinic_f$scores)
+demog_fa <- as.data.frame(demog_f$scores)
+launch_fa <- as.data.frame(launch_f$scores)
+mvmnt_fa <- as.data.frame(mvmnt_f$scores)
+accuracy_fa <- as.data.frame(accuracy_f$scores)
+treat_fa <- as.data.frame(treat)
+
+Factored_data <- cbind(spd_fa, stroke_fa, clinic_fa, demog_fa,
+                       launch_fa, mvmnt_fa,accuracy_fa, treat_fa)
+colnames(Factored_data) <- c("Speed Factor 3", 
+                             "Speed Factor 1", 
+                             "Speed Factor 2",
+                             "Stroke Factor 4", 
+                             "Stroke Factor 3", 
+                             "Stroke Factor 2",
+                             "Stroke Factor 1", 
+                             "Clinical Scores Factor 1", 
+                             "Demographics Factor 1",
+                             "Launch Quality Factor 1", 
+                             "Launch Quality Factor 2", 
+                             "Whole Movement Factor 1",
+                             "Whole Movement Factor 2", 
+                             "Whole Movement Factor 3", 
+                             "Accuracy Factor 1",
+                             "Accuracy Factor 2",
+                             "Treatment Flag")
+
+## set up the parallel pool
+registerDoParallel(cores=4) # 4 cores to do the simulations
+
+predLASSO1<-NULL
+predLASSO2<-NULL
+predLASSO3<-NULL
+
+for (i in 1:100)
+{
+  predLASSO1[[i]] <- cv_mod(feat=Factored_data,out=yFM,stand=1,isWO = 0)
+  predLASSO2[[i]] <- cv_mod(feat=Factored_data,out=yPartFM,stand=1,isWO = 0)
+  predLASSO3[[i]] <- cv_mod(feat=Factored_data,out=yWO,stand=1,isWO = 1)
+}
+LASSO_pred1 <- cleanup_LASSO(predLASSO1)
+LASSO_pred2 <- cleanup_LASSO(predLASSO2)
+LASSO_pred3 <- cleanup_LASSO(predLASSO3)
+
+predRF1 <- NULL
+predRF2 <- NULL
+predRF3 <- NULL
+for (i in 1:100)
+{
+  predRF1[[i]] <- cv_RF(feat=Factored_data, out=yFM)
+  predRF2[[i]] <- cv_RF(feat=Factored_data, out=yPartFM)
+  predRF3[[i]] <- cv_RF(feat=Factored_data, out=yWO)
+}
+
+RF_pred1 <- cleanup_RF(predRF1)
+RF_pred2 <- cleanup_RF(predRF2)
+RF_pred3 <- cleanup_RF(predRF3)
